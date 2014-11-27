291e845bfa173036912260baf4bfbd76d607c793
Jeff Pratt <me@jeffreypratt.net>
2014-11-26 22:49:39 -0700

    Add navbar; spiff up detail page

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class Config(object):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///./data/habcat.sqlite'
 
 
 class DevelopmentConfig(Config):
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask, abort, render_template
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from models import Habstar
+from flask import Flask, abort, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 import os
 import logging
@@ -15,26 +12,55 @@ if app.debug:
 else:
     app.logger.setLevel(logging.WARN)
 
-engine = create_engine('sqlite:///./data/habcat.sqlite')
-Session = sessionmaker(bind=engine)
+db = SQLAlchemy(app)
+
+
+class Habstar(db.Model):
+    __tablename__ = 'habstar'
+
+    hipparchos_num = db.Column(db.Integer, primary_key=True)
+    ra_hours = db.Column(db.Integer)
+    ra_minutes = db.Column(db.Integer)
+    ra_seconds = db.Column(db.Float)
+    dec_degrees = db.Column(db.Integer)
+    dec_minutes = db.Column(db.Integer)
+    dec_seconds = db.Column(db.Float)
+    johnson_mag = db.Column(db.Float)
+    parallax_mas = db.Column(db.Float)
+    sigma_parallax_mas = db.Column(db.Float)
+    b_minus_v = db.Column(db.Float)
+    dist_pc = db.Column(db.Float)
+    x_pc = db.Column(db.Float)
+    y_pc = db.Column(db.Float)
+    z_pc = db.Column(db.Float)
 
 
 @app.route('/')
 def home():
-    session = Session()
-    habstars = session.query(Habstar).limit(25)
+    habstars = Habstar.query.limit(25).all()
 
     return render_template('index.html', habstars=habstars)
 
 
 @app.route('/habstar/<id>')
 def detail(id):
-    session = Session()
-    habstar = session.query(Habstar).get(id)
+    habstar = Habstar.query.get(id)
     if habstar:
         return render_template('detail.html', habstar=habstar)
     else:
         abort(404)
+
+
+@app.route('/find')
+def find():
+    center_hipparchos_num = request.args.get('c')
+    dist_pc = request.args.get('d')
+
+    center_habstar = Habstar.query.get(center_hipparchos_num)
+
+    habstars = Habstar.query.all()
+
+    return render_template('find.html', dist_pc=dist_pc, center_hipparchos_num=center_hipparchos_num, habstars=habstars)
 
 
 @app.errorhandler(404)
